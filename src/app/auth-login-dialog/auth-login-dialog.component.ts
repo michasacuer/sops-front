c0e230c65b9fb5c7b9b65253702764781b8578f8
Michał Szewczak <michasacuer3@gmail.com>
2019-01-03 18:48:08 +0100

    deleting rates (MyProducts)

--- src/app/auth-login-dialog/auth-login-dialog.component.ts
+++ src/app/auth-login-dialog/auth-login-dialog.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
-import { UserCredentials } from '../models/user-credentials';
-import { AuthService } from '../auth.service';
-import { DataResponse } from '../data.service';
+import { Component, OnInit, EventEmitter } from "@angular/core";
+import { MatDialogRef } from "@angular/material";
+import { UserCredentials } from "../models/user-credentials";
+import { AuthService } from "../auth.service";
+import { DataResponse } from "../data.service";
 
 @Component({
-  selector: 'app-auth-login-dialog',
-  templateUrl: './auth-login-dialog.component.html',
-  styleUrls: ['./auth-login-dialog.component.css']
+  selector: "app-auth-login-dialog",
+  templateUrl: "./auth-login-dialog.component.html",
+  styleUrls: ["./auth-login-dialog.component.css"]
 })
 export class AuthLoginDialogComponent implements OnInit {
   public userCredentials: UserCredentials = new UserCredentials();
   public submitEmitter = new EventEmitter();
   public errorMessage: string;
 
-  constructor(public auth: AuthService, public dialogRef: MatDialogRef<AuthLoginDialogComponent>) { }
+  constructor(
+    public auth: AuthService,
+    public dialogRef: MatDialogRef<AuthLoginDialogComponent>
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   onLoginClick() {
     this.submitEmitter.emit();
-    this.auth.signIn(this.userCredentials).subscribe((response) => {
+    this.auth.signIn(this.userCredentials).subscribe(response => {
       if (response.object) {
         this.dialogRef.close(this.userCredentials);
       } else {
@@ -28,5 +30,4 @@ export class AuthLoginDialogComponent implements OnInit {
       }
     });
   }
-
 }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-import { UserInfo } from '../models/user-info';
-import { UserCredentials } from '../models/user-credentials';
-import { MatDialog } from '@angular/material';
-import { AuthLoginDialogComponent } from '../auth-login-dialog/auth-login-dialog.component';
-import { EventEmitter } from 'protractor';
-import { AuthRegisterDialogComponent } from '../auth-register-dialog/auth-register-dialog.component';
-import { clippyRef } from '../../assets/clippy/clippy-ref';
-import { ErrorService } from '../error.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
+import { UserInfo } from "../models/user-info";
+import { UserCredentials } from "../models/user-credentials";
+import { MatDialog } from "@angular/material";
+import { AuthLoginDialogComponent } from "../auth-login-dialog/auth-login-dialog.component";
+import { EventEmitter } from "protractor";
+import { AuthRegisterDialogComponent } from "../auth-register-dialog/auth-register-dialog.component";
+import { clippyRef } from "../../assets/clippy/clippy-ref";
+import { ErrorService } from "../error.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"]
 })
 export class AuthComponent implements OnInit {
   public userInfo: UserInfo;
 
-  constructor(public auth: AuthService, public dialog: MatDialog, public errorService: ErrorService) { }
+  constructor(
+    public auth: AuthService,
+    public dialog: MatDialog,
+    public errorService: ErrorService
+  ) {}
 
   ngOnInit() {
-    this.auth.userInfo.subscribe((response) => {
+    this.auth.userInfo.subscribe(response => {
       // this.errorService.showError(response);
       this.userInfo = response.object;
     });
@@ -39,16 +43,19 @@ export class AuthComponent implements OnInit {
     const dialogRef = this.dialog.open(AuthRegisterDialogComponent);
 
     dialogRef.componentInstance.registerEmitter.subscribe(() => {
-      this.auth.register(dialogRef.componentInstance.userRegister).subscribe((response) => {
-
-        if (response.status === 200) {
-          dialogRef.close();
+      this.auth.register(dialogRef.componentInstance.userRegister).subscribe(
+        response => {
+          if (response.status === 200) {
+            dialogRef.close();
+            clippyRef.stop();
+            clippyRef.speak(`Witam w serwisie przyjacielu.`);
+          }
+        },
+        error => {
           clippyRef.stop();
-          clippyRef.speak(`Witam w serwisie przyjacielu.`);
-        }
-      }, error => {
-        clippyRef.stop();
-        clippyRef.speak(`Taki użytkownik już istnieje, albo coś takiego.      Tak, teraz mówię po polsku.`);
+          clippyRef.speak(
+            `Taki użytkownik już istnieje, albo coś takiego.      Tak, teraz mówię po polsku.`
+          );
         }
       );
     });
