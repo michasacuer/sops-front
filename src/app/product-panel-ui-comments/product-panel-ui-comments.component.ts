eca3403d105c7603b6376abc87aab310fe2e8123
Michał Szewczak <michasacuer3@gmail.com>
2019-01-08 07:46:27 +0100

    Adding comments works. needed to update list after Submit

--- src/app/product-panel-ui-comments/product-panel-ui-comments.component.ts
+++ src/app/product-panel-ui-comments/product-panel-ui-comments.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter } from "@angular/core";
 import { Product } from "../models/product";
+import { DataService, ModelState } from "../data.service";
+import { ErrorService } from "../error.service";
+import { ProductComment } from "../models/product-comment";
+
 @Component({
   selector: "app-product-panel-ui-comments",
   templateUrl: "./product-panel-ui-comments.component.html",
@@ -7,7 +11,43 @@ import { Product } from "../models/product";
 })
 export class ProductPanelUiCommentsComponent implements OnInit {
   @Input() product: Product;
-  constructor() {}
+  commentFromInput: string;
+  submitEmitter = new EventEmitter();
+  modelState = new ModelState();
+  newComment: ProductComment = new ProductComment();
+
+  constructor(
+    private dataService: DataService,
+    private errorService: ErrorService
+  ) {}
 
   ngOnInit() {}
+
+  submitComment(): void {
+    if (this.commentFromInput == null) {
+      console.log("string empty");
+      return;
+    }
+    this.newComment.comment = this.commentFromInput;
+    this.submitEmitter.emit();
+    this.dataService
+      .postObjectByUrl(this.newComment, `api/ProductComment/${this.product.id}`)
+      .subscribe(result => {
+        if (result.object || !result.modelState.isOk()) {
+          this.modelState.update(result.modelState);
+          this.getComments();
+        } else {
+          this.errorService.showError(result);
+        }
+      });
+  }
+
+  getComments() {
+    this.dataService
+      .getObjectsByUrl(ProductComment, `api/ProductComment/${this.product.id}`)
+      .subscribe(result => {
+        this.product.productComments = result.object;
+        console.log(result);
+      });
+  }
 }
