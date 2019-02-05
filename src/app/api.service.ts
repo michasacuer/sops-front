79554f7c62015c93bc4d84ae8fc5824336975b44
Michał Szewczak <michasacuer3@gmail.com>
2019-02-05 19:12:27 +0100

    change api url to localhost

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,7 +8,7 @@ export class ApiService {
   constructor() { }
 
   getBaseUrl(): string {
-    //return 'http://localhost:56971/';
-    return 'http://sops2.azurewebsites.net/';
+    return 'http://localhost:56971/';
+    //return 'http://sops2.azurewebsites.net/';
   }
 }
